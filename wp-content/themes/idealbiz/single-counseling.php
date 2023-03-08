6093521ff4da9ae5f7b9ba78afd87c56e1147d9c
Goncalo Soares <goncalo.soares@tgsportugal.com>
2023-03-08 18:59:17 +0000

    Fix var name

--- wp-content/themes/idealbiz/single-counseling.php
+++ wp-content/themes/idealbiz/single-counseling.php
@@ -769,7 +769,7 @@ $p .= '<span id="result_D" class="cl_aviso" ></span>';
             function insertTooltips() {
                 $(".gfield").each(function(index, element) {
                     var gfFieldId = (element.id || '').split('_')[2];
-                    if(gfFieldId && gfField !== '11') {
+                    if(gfFieldId && gfFieldId !== '11') {
                         $(element)
                             .append('<div class=gfield_label><button class="info-balloon">i</button>')           //Coloca os "i's" ao lado dos campos.
                             .find('button')
