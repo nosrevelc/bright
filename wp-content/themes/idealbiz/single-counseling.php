072a8ebe95ddd212976620c4437dca395f87ec46
Goncalo Soares <goncalo.soares@tgsportugal.com>
2023-03-08 19:05:41 +0000

    Append button to label

--- wp-content/themes/idealbiz/single-counseling.php
+++ wp-content/themes/idealbiz/single-counseling.php
@@ -771,6 +771,7 @@ $p .= '<span id="result_D" class="cl_aviso" ></span>';
                     var gfFieldId = (element.id || '').split('_')[2];
                     if(gfFieldId && gfFieldId !== '11') {
                         $(element)
+                            .find('label')
                             .append('<div class=gfield_label><button class="info-balloon">i</button>')           //Coloca os "i's" ao lado dos campos.
                             .find('button')
                             .click(function(event) { event.preventDefault(); $(`#tooltip_${gfFieldId}`).iziModal('open'); })    //Chama o iziModal onClick
